4b07bb2309b76d3e805bcd32086fa6b8afcbcf27
Oceane Lonneux <oceanelonneux@gmail.com>
2018-09-17 16:44:31 +0100

    commented blog

--- test/test-server.js
+++ test/test-server.js
@@ -35,8 +35,6 @@ describe('Homepage', function() {
 //   });
 // });
 
-
-
 describe('Fibonacci', function() {
   it('should display the correct fibonacci value at /fibonacci/10 GET', function(done) {
     chai.request(server)
